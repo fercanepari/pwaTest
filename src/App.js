ab6db8403911b0fbb513e0a2864b03e297b20a59
Fernando <fercanepari@gmail.com>
2020-11-03 21:07:09 -0300

    Route added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './App.css';
 import {Navbar, Nav} from 'react-bootstrap'
-
+import Home from './Home'
+import Users from './Users'
+import About from './About'
+import {Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-      </Navbar>
+      <Router>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/users">Users</Link></Nav.Link>
+            <Nav.Link><Link to="/about">About</Link></Nav.Link>
+          </Nav>
+        </Navbar>
+        <Switch>
+        <Route path="/about" component={About} ></Route>
+        <Route path="/users" component={Users} ></Route>
+        <Route path="/" component={Home} ></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
