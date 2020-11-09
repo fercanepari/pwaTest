737303f744d61750589e24f2f0fee43ce1c75b7a
Fernando <fercanepari@gmail.com>
2020-11-09 01:48:06 -0300

    Detect installed app

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Home() {
+
+  const [mode, setMode]=useState('web')
+
+  if (window.matchMedia('(display-mode: standalone)').matches) {
+    setMode('standalone')
+  }
+
     return (
+      <div>
+        <div>
+            {
+              mode==='web'?
+              <div class="alert alert-warning" role="alert">
+                Usando el navegador!
+              </div>
+              :
+              <div class="alert alert-warning" role="alert">
+                App instalada!
+              </div>
+            }
+          </div>
         <div>
           <h1>Home component</h1>
         </div>
+      </div>
       );
 }
